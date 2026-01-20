4df9170ba55329c718b3f09bf789687e955b207a
Génesis V. | Developer Student <g.valdebenitoquintupil@gmail.com>
2026-01-20 15:42:22 -0300

    Create Comentarios.md

---
+++ Comentarios.m
@@ -0,0 +1,5 @@
+# Comentarios
+- Se realizó un fork del repositorio entregado: https://github.com/gsanchezd/fdsw-github. **(3 puntos)**
+- A partir de la plantilla entregada, se modificó el CV agregando los datos personales especificados. **(3 puntos)**
+- Se realizaron al menos 5 commits con modificaciones al repositorio forkeado. **(3 puntos)**
+- El link del repositorio personal es compartido en el lms, al cual se puede acceder y visualizar sin problemas. **(1 puntos)**
